a13ac82695829f6daba44bbe4833cf29e2cd1585
Codec04 <90425490+Coosta6915@users.noreply.github.com>
2022-01-10 21:54:39 +0000

    Removed themes (temporarily)

--- pycos/src/system/gooey/manager.py
+++ pycos/src/system/gooey/manager.py
@@ -3,21 +3,20 @@ import picodisplay as screen
 import time
 
 from config import *
-from themes.raspberry import *
 
 
 def clear():
-    screen.set_pen(BACKGROUND_COLOUR[0], BACKGROUND_COLOUR[1], BACKGROUND_COLOUR[2])
+    screen.set_pen(255, 0, 0)
     screen.clear()
 
 
 def text(string: str, x: int, y: int, size: int = 2):
-    screen.set_pen(TEXT_COLOUR[0], TEXT_COLOUR[1], TEXT_COLOUR[2])
+    screen.set_pen(255, 255, 255)
     screen.text(string, x, y, (screen.get_width() - x), size)
 
 
 def separator():
-    screen.set_pen(SEPARATOR_COLOUR[0], SEPARATOR_COLOUR[1], SEPARATOR_COLOUR[2])
+    screen.set_pen(255, 85, 85)
     screen.circle(15, int(screen.get_height() / 2), 1)
     screen.rectangle(15, int((screen.get_height() / 2) - 2), int(screen.get_width() - 30), 4)
     screen.circle(int(screen.get_width() - 15), int(screen.get_height() / 2), 1)
--- pycos/src/themes/raspberry.py
+++
@@ -1,8 +0,0 @@
-TEXT_COLOUR = (255, 255, 255)
-# text colour
-
-BACKGROUND_COLOUR = (255, 0, 0)
-# background colour
-
-SEPARATOR_COLOUR = (255, 85, 85)
-# separator colour
